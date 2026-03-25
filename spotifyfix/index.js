5c0d4c0fea53e5c61edc212c7085f5d76fd7f468
ehte8989 <157400446+ehte8989@users.noreply.github.com>
2026-03-25 09:40:20 +0000

    Update index.js

--- spotifyfix/index.js
+++ spotifyfix/index.js
@@ -1,1 +1,25 @@
-(function(e,o,i){"use strict";const a=o.findByProps("dispatch");let t;var d={onLoad:function(){t=i.before("dispatch",a,function(u){let[n]=u;n.type==="SPOTIFY_PLAYER_STATE"&&n.payload?.isPremium===0&&(n.payload.isPremium=!0)})},onUnload:function(){typeof t=="function"&&t()}};return e.default=d,Object.defineProperty(e,"__esModule",{value:!0}),e})({},vendetta.metro,vendetta.patcher);
+import { findByProps } from "@vendetta/metro";
+import { before } from "@vendetta/patcher";
+
+const SpotifyStore = findByProps("dispatch")
+let unpatch
+
+export default {
+
+    onLoad: () => {
+        unpatch = before("dispatch", SpotifyStore, ([action]) => {
+            if (
+                action.type === "SPOTIFY_PLAYER_STATE" &&
+                action.payload?.isPremium === false
+            ) {
+                action.payload.isPremium = true;
+            }
+        });
+    },
+
+    onUnload: () => {
+        if (typeof unpatch === "function") {
+            unpatch();
+        }
+    },
+}
